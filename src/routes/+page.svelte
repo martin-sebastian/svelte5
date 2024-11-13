ea4db032f29a616c249b78b55bcca0fe694bd405
Martin Sebastian <martin-sebastian@users.noreply.github.com>
2024-11-13 17:58:08 -0500

    Update page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
-<h1 class="text-2xl font-bold">Welcome to SvelteKit</h1>
-<p class="text-lg font-semibold">
-	Demo: <a href="/demo/lucia">Lucia Demo</a>
-</p>
-<p class="text-lg font-semibold">
-	Drizzle: <a href="https://local.drizzle.studio/">Drizzle Studio</a>
-</p>
+<div class="flex flex-col items-center">
+	<h1 class="my-5 text-2xl font-bold">Welcome to SvelteKit</h1>
+</div>
+<div class="flex justify-center">
+	<a class="mx-1 rounded-md bg-blue-500 px-2 py-2 text-white" href="/demo/lucia">Lucia Demo</a>
+	<a class="mx-1 rounded-md bg-red-500 px-2 py-2 text-white" href="https://local.drizzle.studio/"
+		>Drizzle Studio</a
+	>
+</div>
