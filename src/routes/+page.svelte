74ad422a27fe890fbdc0bd67181e48648e2488d8
Martin Sebastian <martin@martinsebastian.co>
2025-01-18 11:35:07 -0500

    Update home page to resolve vercel errors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,20 @@
-<script>
+<script lang="ts">
+	import type { ComponentType } from 'svelte';
+	import DiJsBadge from 'svelte-icons/di/DiJsBadge.svelte';
+	import DiHtml5 from 'svelte-icons/di/DiHtml5.svelte';
+	import DiCss3Full from 'svelte-icons/di/DiCss3Full.svelte';
+	import DiBootstrap from 'svelte-icons/di/DiBootstrap.svelte';
+	import DiAngularSimple from 'svelte-icons/di/DiAngularSimple.svelte';
+	import DiGithubBadge from 'svelte-icons/di/DiGithubBadge.svelte';
+	import DiMysql from 'svelte-icons/di/DiMysql.svelte';
+	import DiPostgresql from 'svelte-icons/di/DiPostgresql.svelte';
+	import DiSqllite from 'svelte-icons/di/DiSqllite.svelte';
+
+	interface IconData {
+		name: string;
+		icon: ComponentType;
+	}
+
 	let message = 'Dealership Power Tools for Dealer Spike';
 	let subMessage = 'Svelte 5, Drizzle, Lucia, SQLite, Tailwind, TypeScript, Shadcn UI.';
 	let technologyIcons = [
@@ -31,25 +47,6 @@
 		{ name: 'MySQL', icon: DiMysql },
 		{ name: 'PostgreSQL', icon: DiPostgresql }
 	];
-
-	// @ts-ignore
-	import DiJsBadge from 'svelte-icons/di/DiJsBadge.svelte';
-	// @ts-ignore
-	import DiHtml5 from 'svelte-icons/di/DiHtml5.svelte';
-	// @ts-ignore
-	import DiCss3Full from 'svelte-icons/di/DiCss3Full.svelte';
-	// @ts-ignore
-	import DiBootstrap from 'svelte-icons/di/DiBootstrap.svelte';
-	// @ts-ignore
-	import DiAngularSimple from 'svelte-icons/di/DiAngularSimple.svelte';
-	// @ts-ignore
-	import DiGithubBadge from 'svelte-icons/di/DiGithubBadge.svelte';
-	// @ts-ignore
-	import DiMysql from 'svelte-icons/di/DiMysql.svelte';
-	// @ts-ignore
-	import DiPostgresql from 'svelte-icons/di/DiPostgresql.svelte';
-	// @ts-ignore
-	import DiSqllite from 'svelte-icons/di/DiSqllite.svelte';
 </script>
 
 <main class="align-start flex h-full min-h-screen w-full flex-col items-center">
@@ -63,7 +60,14 @@
 	<div class="mb-10 flex justify-center text-2xl text-gray-500/75">
 		{subMessage}
 	</div>
-
+	<div class="my-10 flex flex-col items-center justify-start">
+		<a
+		href="/admin/vehicles"
+		class="my-10 rounded-xl border border-gray-400/25 bg-gradient-to-r from-gray-400/50 to-blue-500/50 bg-clip-text px-8 py-4 text-lg font-bold text-transparent"
+		>SIGN UP NOW</a
+		>
+		<img src="/vehicles-screen.png" alt="Vehicles Screen" class="w-2/3" />
+	</div>
 	<div class="max-w-8xl hidden">
 		<div class="grid grid-cols-3 gap-4 xl:grid-cols-4">
 			<div class="col-span-2 h-36 w-full rounded-xl border border-gray-400/25 bg-background">
@@ -111,13 +115,5 @@
 				<div class="p-4">06</div>
 			</div>
 		</div>
-	</div>
-	<div class="my-10 flex flex-col items-center justify-start">
-		<img src="/vehicles-screen.png" alt="Vehicles Screen" class="w-2/3" />
-		<a
-			href="/admin/vehicles"
-			class="my-10 rounded-xl border border-gray-400/25 bg-gradient-to-r from-gray-400/50 to-blue-500/50 bg-clip-text px-8 py-4 text-lg font-bold text-transparent"
-			>LOGIN NOW</a
-		>
 	</div>
 </main>
