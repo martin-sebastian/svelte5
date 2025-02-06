956d6e60049f7ef69973c0fd74fc3c077e3d4e2f
Martin Sebastian <martin@martinsebastian.co>
2025-02-06 02:57:02 -0500

    Update Home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
+	import { CircleGauge } from 'lucide-svelte';
 </script>
 
 <div class="container mx-auto flex min-h-screen flex-col items-center justify-center">
@@ -8,6 +9,9 @@
 	<div class="space-x-4">
 		<Button href="/auth/login" variant="default">Login</Button>
 		<Button href="/auth/register" variant="outline">Register</Button>
-		<Button href="/admin" variant="ghost">Admin Dashboard</Button>
+		<Button href="/admin">
+			<CircleGauge class="mr-2 h-4 w-4" />
+			Admin Dashboard
+		</Button>
 	</div>
 </div>
